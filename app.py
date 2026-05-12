9ed845424d751d58b9297e99e0f830aa61c2bec1
kidbradoki <kidbradoki@gmail.com>
2026-05-12 12:27:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,6 @@ H = """
 """
 
 @app.route('/')
-def index(): return render_template_string(H)
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7860)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
